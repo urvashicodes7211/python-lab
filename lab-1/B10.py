c7960f411927e21b20740c2b04399b600ed176a0
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-07-27 23:58:45 +0530

    calculate simple interest

---
+++ lab-1/B10.py
@@ -0,0 +1,6 @@
+# Write a program to calculate simple interest.
+
+p = float(input ("Enter p: "))
+r = float(input ("Enter r: "))
+n = float(input ("Enter n: "))
+print("simple interest : ",(p*r*n)/100)
