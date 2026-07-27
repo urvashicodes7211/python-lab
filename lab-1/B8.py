662cd81cff71502a2daf6a45f40dc4e28f500bd7
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-07-27 23:55:35 +0530

    area of a triangle

---
+++ lab-1/B8.py
@@ -0,0 +1,5 @@
+# Write a program to calculate the area of a triangle (hint: a = h*b*0.5).
+
+h = float(input ("Enter height: "))
+b = float(input ("Enter base: "))
+print("area of a triangle : ",h*b*0.5)
