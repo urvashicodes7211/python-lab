eee0b433bbd294d0e94463441d233807dcf5edc0
Urvashi Zinzuvadiya <urvashi7211@gmail.com>
2026-08-01 16:17:08 +0530

    find the largest number using ternary operator

---
+++ lab-2/B6.py
@@ -0,0 +1,7 @@
+'''Write a program to find the largest number from the given two numbers using the 
+ternary operator.'''
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+print("largest number :",a) if(a>b) else print("largest number :",b)
